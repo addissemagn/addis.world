11a19787282f26fda94fed81c3ef9e4df218aeb0
Addis Semagn <asemagn@gmail.com>
2021-05-09 11:51:22 -0400

    Update projects

--- data/projects.js
+++ data/projects.js
@@ -7,17 +7,6 @@ const data = {
   },
   projects: [
     {
-      name: "ChainReact",
-      path: "/assets/projects-chainreact.gif",
-      stack: [
-        "Python",
-        "TypeScript",
-        "Blockchain using <a href='https://docs.ont.io'>Ontology</a> for smart contract development",
-      ],
-      desc:
-        "<b>Winner @ CryptoChicks Hack 2019</b> — Emergency response system connecting bystanders of cardiac arrest to a network of CPR certified volunteers.",
-    },
-    {
       name: "Yeshi Social Cookbook",
       path: "/assets/projects-yeshi.gif",
       stack: [
@@ -30,7 +19,18 @@ const data = {
         "Netlify serverless functions",
       ],
       desc:
-        "A social cookbook manager I made for my families to share our home recipes. <strong>Features</strong>: upload recipes from handwriting, add images, create and share cookbooks, track pantry items, add recipe to grocery list to auto update missing ingredients.",
+        "A social cookbook manager. <strong>Motivation</strong>: I made this for my families to share our home recipes. <strong>Features</strong>: upload and convert handwritten recipes to text, create and share cookbooks with user permissions, add images, track items you have in stock, add recipe to grocery list to auto update ingredients you're missing.",
+    },
+    {
+      name: "ChainReact",
+      path: "/assets/projects-chainreact.gif",
+      stack: [
+        "Python",
+        "TypeScript",
+        "Blockchain using <a href='https://docs.ont.io'>Ontology</a> for smart contract development",
+      ],
+      desc:
+        "<b>Winner @ CryptoChicks Hack 2019</b> — Emergency response system connecting bystanders of cardiac arrest to a network of CPR certified volunteers.",
     },
     {
       name: "SnapSaver",
@@ -40,18 +40,7 @@ const data = {
         "Simple web and CLI tool to bulk download your Snapchat Memories. <strong>Motivation</strong>: Snapchat makes you download your media one by one. As there's no tool to mass download them, I built one. <strong>Features</strong>: resuming download, file and ZIP handling, optionally receiving email with ZIP once complete.",
     },
     {
-      name: "Spotify-themed Personal Website",
-      path: "/assets/projects-spotify.gif",
-      stack: [
-        "JavaScript",
-        "CSS",
-        "<a href='https://github.com/dragonman225/notablog'>Notablog</a>",
-      ],
-      desc:
-        "This was version 23497 of my website (I've lost count), integrated with Notion as the CMS. Archived for now but my personal favorite :)",
-    },
-    {
-      name: "Cannonball Website",
+      name: "UofT Cannonball Website",
       path: "/assets/projects-cannonball.gif",
       stack: [
         "React",
@@ -63,21 +52,13 @@ const data = {
         "Netlify serverless functions",
       ],
       desc:
-        "Event/ticketing app for University of Toronto engineering event. <strong>Features not shown</strong>: Choose Your Adventure game, admin dashboard.",
+        "Event + ticketing app for a University of Toronto engineering event. <strong>Features</strong>: Choose Your Adventure game, admin dashboard, Stripe payments and Gmail confirmation emails.",
     },
     {
-      name: "Educate Me",
-      path: "/assets/projects-educate.jpg",
-      stack: ["JavaScript", "Node.js"],
-      desc:
-        "Explore the depth of internet knowledge, one randomly generated TED Talk or article at a time.",
-    },
-    {
-      name: "GIS Mapping Desktop App",
-      path: "/assets/projects-mapper.gif",
-      stack: ["C++", "EZGL UI", "OpenStreetMap data"],
-      desc:
-        "GIS city mapping application, implementing pathfinding algorithm based on A* Search and Dijkstra’s algorithms.",
+      name: "UofT Locker Rental",
+      path: "/assets/projects-lockers.png",
+      stack: ["MySQL", "PHP (limited Apache servers, what can ya do)", "Material UI", "JavaScript"],
+      desc: "Locker rental booking website for the University of Toronto Engineering Society, overhauling previous system. <strong>Features</strong>: user registration + login, booking system, user dashboard for managing profile and locker status, and admin dashboard for managing students, lockers, booking approval, and admin permissions.",
     },
     {
       name: "Othello AI",
@@ -89,8 +70,22 @@ const data = {
     {
       name: "Rubik",
       path: "/assets/projects-rubik.jpg",
-      stack: ["JavaScript", "Three.js", "jQuery"],
+      stack: ["JavaScript", "<a href='https://threejs.org'>Three.js</a> for 3D graphics", "jQuery"],
       desc: "Interactive 3D rubik's cube with built-in solver.",
+    },
+    {
+      name: "Educate Me",
+      path: "/assets/projects-educate.jpg",
+      stack: ["JavaScript", "Node.js for web crawling"],
+      desc:
+        "Explore the depth of internet knowledge, one randomly generated TED Talk or article at a time.",
+    },
+    {
+      name: "GIS Mapping Desktop App",
+      path: "/assets/projects-mapper.gif",
+      stack: ["C++", "EZGL UI", "OpenStreetMap data"],
+      desc:
+        "GIS city mapping application, implementing pathfinding algorithm based on A* Search and Dijkstra’s algorithms.",
     },
     {
       name: "Weather Genie",
@@ -98,25 +93,36 @@ const data = {
       stack: ["React", "JavaScript", "Meteostat API", "Unsplash API"],
       desc: "Compare weather across cities.",
     },
+    // {
+    //   name: "Hangry UofT",
+    //   path: "/assets/projects-hangry.gif",
+    //   stack: ["JavaScript", "MySQL", "Google Maps API", "CSS"],
+    //   desc:
+    //     "A Yelp-style website of all dining options at University of Toronto St. George.",
+    // },
     {
-      name: "Hangry UofT",
-      path: "/assets/projects-hangry.gif",
-      stack: ["JavaScript", "MySQL", "Google Maps API", "CSS"],
-      desc:
-        "A Yelp-style website of all dining options at University of Toronto St. George.",
-    },
-    {
-      name: "Personal Website",
-      path: "/assets/projects-personal-vue.png",
+      name: "Spotify-themed Personal Website",
+      path: "/assets/projects-spotify.gif",
       stack: [
         "JavaScript",
+        "CSS",
+        "<a href='https://github.com/dragonman225/notablog'>Notablog</a>",
+      ],
+      desc:
+        "Version 23497 of my website (I've lost count) and my personal favorite! I wanted to integrate Notion as the CMS since it's my go-to for everything. Spotify is my love and adding the playable song is a lil homage to my Tumblr blog customization days :)",
+    },
+    {
+      name: "Yet Another Personal Website",
+      path: "/assets/projects-personal-vue.png",
+      stack: [
         "Vue",
+        "JavaScript",
         "<a href='https://github.com/eggplanetio/gustavo'>Gustavo</a>",
         "AWS EC2",
         "CSS",
       ],
       desc:
-        "Yet another one. This one I wanted to use Github's gists for my writing and self-host using AWS EC2.",
+        "I had just discovered the joys of Github's gists so I wanted to use it for my writing and also experiment with a self-hosting setup with AWS EC2.",
     },
   ],
 };
