1d9b72efa0325a2c411c7ba8fd586767876fb442
Addis Semagn <asemagn@gmail.com>
2021-08-06 21:15:41 -0400

    Add resume page 🤠 (#10)
    Co-authored-by: Addis Semagn <t-asemagn@microsoft.com>

---
+++ src/data/resume.js
@@ -0,0 +1,9 @@
+const data = {
+  resume: {
+    preview_link: "https://drive.google.com/file/d/1jpTGtWO7QelAqqyAxooFjeLmHeng8_76/preview",
+    direct_download_text: "Download Resume",
+    direct_download_link: "https://drive.google.com/uc?export=download&id=1jpTGtWO7QelAqqyAxooFjeLmHeng8_76",
+  },
+};
+
+export default data;
---
+++ src/pages/resume.js
@@ -0,0 +1,27 @@
+import Nav from "../components/Nav.js";
+import Footer from "../components/Footer.js";
+import { Header, Styled } from "../components/Misc.js";
+import data from "../data/resume.js";
+
+const SectionResume = () => (
+  <div class='embed-container'><iframe src={data.resume.preview_link} width='100%' height='100%'></iframe></div>
+)
+
+const App = () => {
+  return (
+    <div class="wrapper">
+      <main>
+        <Nav />
+        <Header
+	  heading="ʕ•ᴥ•ʔ"
+	  subheading={<Styled text={`<a href='${data.resume.direct_download_link}'>${data.resume.direct_download_text}</a>`} />} 
+	  subscript="Last updated Aug 6, 2021"
+	 />
+	<SectionResume />
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default App;
